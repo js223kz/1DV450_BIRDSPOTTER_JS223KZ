b0a113e015b91cfed01a931305fab5e7508ea344
Johanna Szepanski <js223kz@student.lnu.se>
2016-01-25 15:56:08 +0000

    added a User model

--- birdspotter/db/schema.rb
+++ birdspotter/db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125142932) do
+ActiveRecord::Schema.define(version: 20160125154654) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "username"
+    t.string   "password"
   end
 
 end
