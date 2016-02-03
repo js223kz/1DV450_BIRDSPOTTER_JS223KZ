5d8178f3b00463f848ba96612f27391994c8a2e6
Johanna Szepanski <js223kz@student.lnu.se>
2016-02-03 15:10:48 +0000

    added routes and layout

---
+++ birdspotter/app/controllers/start_controller.rb
@@ -0,0 +1,7 @@
+class StartController < ApplicationController
+    def start
+    end
+    
+    def signup 
+    end
+end
--- birdspotter/config/routes.rb
+++ birdspotter/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   #root 'welcome#index'
+  root  'start#start'
+  get 'login' => 'start#login'
+  get 'signup' => 'users#new'
+
   
   get "start/" => "start#index"
   # Example of regular route:
