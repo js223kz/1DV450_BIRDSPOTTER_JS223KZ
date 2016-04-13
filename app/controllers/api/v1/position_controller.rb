64aa6b38dbd298867b59710dce0673a5c5bad599
Johanna Szepanski <js223kz@student.lnu.se>
2016-04-13 09:51:17 +0000

    removed totalcount from nearby method

--- app/controllers/api/v1/position_controller.rb
+++ app/controllers/api/v1/position_controller.rb
@@ -7,8 +7,7 @@ class Api::V1::PositionController < ApplicationController
         @spots = Api::V1::Spot.near([@lat, @lng], @offset, :units => :km)
         render json:{
             message: "200 OK",
-            totalCount: @spots.count,
-            spots:ActiveModel::ArraySerializer.new(@spots, each_serializer: Api::V1::SpotSerializer) 
+            spots: ActiveModel::ArraySerializer.new(@spots, each_serializer: Api::V1::SpotSerializer) 
         }, status: 200
     end
 end
