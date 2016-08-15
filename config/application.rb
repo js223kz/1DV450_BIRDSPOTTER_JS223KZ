8eecb2ad874f1058f5543daba4f3baea4b61bbcc
Johanna Szepanski <js223kz@student.lnu.se>
2016-08-15 07:59:44 +0000

    added permitted methods to CORS

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,26 @@ Bundler.require(*Rails.groups)
 
 module Birdspotter
   class Application < Rails::Application
-    
+
+    # Rails 3/4
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => :any
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
-  end
+    end
+
+    # Rails 5
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
