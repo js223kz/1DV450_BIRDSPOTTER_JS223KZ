febe0573b769038f8236217bef34333ce0fefa69
Johanna Szepanski <js223kz@student.lnu.se>
2016-08-19 11:55:06 +0000

    removed a straying ,

--- app/controllers/api/v1/position_controller.rb
+++ app/controllers/api/v1/position_controller.rb
@@ -4,35 +4,35 @@ class Api::V1::PositionController < ApplicationController
     before_filter :authenticate_developer_key
     
     def spots_nearby
-        lat = params[:lat]
-        lng = params[:lng]
-        offset = params[:offset]
-        
-        if lat.blank?
+        @lat = params[:lat]
+        @lng = params[:lng]
+        @offset = params[:offset]
+
+        if @lat.blank?
            render json:{
                 status: 400,
-                message: "Latitud måste anges.",
+                message: "Latitud måste anges."
             }  
         end
         
-        if lng.blank?
+        if @lng.blank?
            render json:{
                 status: 400,
-                message: "Longitude måste anges.",
+                message: "Longitude måste anges."
             }  
         end
         
-        if offset.blank?
+        if @offset.blank?
            render json:{
                 status: 400,
-                message: "Ett avstånd i km måste anges.",
+                message: "Ett avstånd i km måste anges."
             }  
         end
-        spots = Api::V1::Spot.near([lat, lng], offset, :units => :km)
+        @spots.near([@lat, @lng], @offset, :units => :km)
         render json:{
             status: 200,
             message: "200 OK",
-            spots: ActiveModel::ArraySerializer.new(spots, each_serializer: Api::V1::SpotSerializer) 
+            spots: ActiveModel::ArraySerializer.new(@spots, each_serializer: Api::V1::SpotSerializer) 
         } 
     end
 end
