5ee8507af088f113d6b6c23da46a958d1a2c9afe
Johanna Szepanski <js223kz@student.lnu.se>
2016-08-25 08:11:52 +0000

    fixed a stupid bug in spots model

--- app/models/api/v1/spot.rb
+++ app/models/api/v1/spot.rb
@@ -2,7 +2,6 @@ class Api::V1::Spot < ActiveRecord::Base
     belongs_to :birdspotter
     has_and_belongs_to_many :birds
     reverse_geocoded_by :latitude, :longitude
-    after_validation :reverse_geocode
     default_scope -> { order(created_at: :desc) }
 
     validates   :latitude,
